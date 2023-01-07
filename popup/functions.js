233dd23d5a98629e8d98dafb5bed8201995e7885
Unknown <keyroos@gmail.com>
2023-01-07 19:59:51 +0100

    Dice roller library added

--- popup/functions.js
+++ popup/functions.js
@@ -1,88 +1,78 @@
-export const inputRoll = (e) => {
-  rollString(e.target.value);
-  e.target.value = "";
-};
-
-/**
- * Finds the total value for a roll string input
- * @param {string} input 1d6+2
- * @returns {object} { input: string; total: number; }
- */
-export const rollString = (input) => {
-  console.log(input)
-  const rolls = input.match(/(\d*d\d+)|(\d+)|(\+|-)/g);
-  let outcome = "";
-  const operate = (op, val, total) => (op === "+") ? total+val : total-val;
-
-  let result = 0;
-  let operator = "+";
-  for(const roll of rolls) {
-    // Roll 
-    if(roll.match(/d/g)) {
-      const rolled = rollDice(roll);
-      const rolls = [];
-      for(let r of rolled.rolls) {
-         rolls.push(
-          (r === 1) 
-            ? `<span class="die fail">${r}</span>` 
-            : (r == rolled.die) 
-            ? `<span class="die success">${r}</span>` 
-            : `<span class="die">${r}</span>`
-         );
-      }
-      outcome += rolls.join(" ");
-      result = operate(operator, parseInt(rolled.total), result);
-    }
-    // Modifier
-    if(roll.match(/^\d*$/g)) {
-      outcome += roll;
-      result = operate(operator, parseInt(roll), result);
-    }
-    // Operator
-    if(roll.match(/^(\+|-){1}$/g)) {
-      outcome += ` ${roll} `;
-      operator = roll;
-    }
-  }
-  storeRoll({
-    input,
-    outcome,
-    result
-  });
-};
-
-/**
- * Rolls a string input
- * @param {string} input 1d6
- * @returns {object} { rolls: number[]; total: number; }
- */
-export const rollDice = (input) => {
-  const roll = input.split("d");
-  const count = roll[0] || 1;
-  const die = roll[1];
+let quickRolls = {};
+export const quickRoll = (e) => {
   const rolls = [];
-
-  for(let i = 1; i <= count; i++) {
-    rolls.push(Math.ceil(Math.random() * die));
+  for(const [die, count] of Object.entries(quickRolls)) {
+      rolls.push(`${count}d${die}`);
   }
-  const total = rolls.reduce((a, b) => a + b);
+  inputRoll(rolls.join("+"));
+  quickRolls = {};
+  e.currentTarget.classList.add("hidden");
+  const dice = document.querySelectorAll("#quick-rolls button");
+  for(const die of dice) {
+      const counter = die.querySelector(".count");
+      if(counter) die.removeChild(counter);
+  }
+};
 
-  return { count, die, rolls, total };
+export const setQuickRoll = (e, operator) => {
+  const quick_btn = document.getElementById("quick-roll-button");
+  const btn = e.currentTarget;
+  const die = btn.getAttribute("data-die");
+  const current = quickRolls[die] || 0;
+
+  if(operator === "-") {
+      e.preventDefault();
+      quickRolls[die] = (current > 0) ? current - 1 : 0;
+  } else {
+      quickRolls[die] = (current < 9) ? current + 1 : 9;
+  }
+
+  const counter = btn.querySelector(".count");
+  if(counter) {
+      if(quickRolls[die] == 0) {
+          btn.removeChild(counter);
+      } else {
+          counter.innerText = quickRolls[die];
+      }
+  } else if(quickRolls[die]) {
+      const count = document.createElement("div");
+      count.setAttribute("class", "count");
+      count.innerText = quickRolls[die];
+      btn.appendChild(count);
+  }
+
+  // Show the Roll button
+  if(Object.values(quickRolls).some(val => val > 0)) {
+      quick_btn.classList.remove("hidden");
+  } else {
+      quick_btn.classList.add("hidden");
+  }
 };
 
 /**
  * Store rolls
  */
 export const storeRoll = (roll) => {
+  const new_roll = {
+      total: roll.total,
+      averageTotal: roll.averageTotal,
+      maxTotal: roll.maxTotal,
+      minTotal: roll.minTotal,
+      notation: roll.notation,
+      output: roll.output,
+      rolls: roll.rolls,
+      total: roll.total
+  };
+
   // Store in Chrome
   chrome.storage.session.get(["rolls"], (result) => {
-    const current = result.rolls || [];
-    current.push(roll);
-    chrome.storage.session.set({ rolls: current });
+      const current = result.rolls || [];
+      current.push(new_roll);
+      chrome.storage.session.set({ rolls: current });
   });
 
   // Add to HTML list
-  addRoll(roll);
+  addRoll(new_roll);
 }
 
 export const addRoll = (roll) => {
@@ -90,26 +80,36 @@ export const addRoll = (roll) => {
   const li = document.createElement("li");
   li.setAttribute("class", "roll");
 
-  const input = document.createElement("div");
-  input.setAttribute("class", "roll__input");
-  input.innerText = roll.input;
+  const notation = document.createElement("div");
+  notation.setAttribute("class", "roll__notation");
+  notation.innerText = roll.notation;
 
   const result = document.createElement("div");
   result.setAttribute("class", "roll__result");
 
-  const outcome = document.createElement("div");
-  outcome.setAttribute("class", "roll__result-outcome");
-  outcome.innerHTML = roll.outcome;
+  const output = document.createElement("div");
+  output.setAttribute("class", "roll__result-output");
+  output.innerHTML = roll.output.replace(/.+:(.*)=.+/g, "$1").trim();
 
   const total = document.createElement("div");
   total.setAttribute("class", "roll__result-total");
-  total.innerText = roll.result;
+  total.innerText = roll.total;
 
-  result.appendChild(outcome);
+  result.appendChild(output);
   result.appendChild(total);
 
-  li.appendChild(input);
+  li.appendChild(notation);
   li.appendChild(result);
 
   list.prepend(li);
 }
+
+export const inputRoll = (notation) => {
+  try {
+      const roll = new rpgDiceRoller.DiceRoll(notation);
+      storeRoll(roll);
+      document.getElementById("roll-string").value = "";
+  } catch(e) {
+      console.error(e);
+  }
+};
--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,8 @@
-import { inputRoll, rollString, addRoll } from "./functions.js";
+import { inputRoll, quickRoll, setQuickRoll, addRoll } from "./functions.js";
 
 const input = document.getElementById("roll-string");
 input.focus();
-input.addEventListener("keypress", (e) => { if(e.key === "Enter") { inputRoll(e) }});
+input.addEventListener("keypress", (e) => { if(e.key === "Enter") { inputRoll(e.target.value) }});
 
 chrome.storage.session.get(["rolls"], (result) => {
   const rolls = result.rolls || [];
@@ -13,12 +13,17 @@ chrome.storage.session.get(["rolls"], (result) => {
 });
 
 // Quick Rolls
+const quick_btn = document.getElementById("quick-roll-button");
+quick_btn.addEventListener("click", quickRoll);
+
 const quick_rolls = document.getElementById("quick-rolls");
-for(const d of [4, 6, 8, 10, 12, 20, 100]) {
+for(const d of [20, 4, 6, 8, 10, 100, 12]) {
   const die = document.createElement("button");
-  die.setAttribute("class", "roll-button");
-  die.addEventListener("click", () => rollString(`1d${d}`));
+  die.setAttribute("class", `roll-button d${d}`);
+  die.setAttribute("data-die", d);
+  die.addEventListener("click", (e) => setQuickRoll(e, "+"));
+  die.addEventListener("contextmenu", (e) => setQuickRoll(e, "-"));
   die.innerText = `d${d}`;
 
   quick_rolls.appendChild(die);
-}
+};
